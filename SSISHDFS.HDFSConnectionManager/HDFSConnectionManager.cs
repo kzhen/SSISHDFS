c762948c4c01352cf45c08beb3f78cbcb18d550b
Ken <dev@kenpire.com>
2013-08-28 11:00:53 +0100

    Added solution

---
+++ SSISHDFS.HDFSConnectionManager/HDFSConnectionManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Hadoop.WebHDFS;
+using Microsoft.SqlServer.Dts.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSISHDFS.HDFSConnectionManager
+{
+  [DtsConnection(ConnectionType="HDFS", DisplayName="HDFS Connection Manager", 
+    Description="Connection Manager for HDFS")]
+  public class HDFSConnectionManager : ConnectionManagerBase
+  {
+    public string UserName { get; set; }
+    public string Path { get; set; }
+
+    public override DTSExecResult Validate(IDTSInfoEvents infoEvents)
+    {
+      if (string.IsNullOrWhiteSpace(UserName))
+      {
+        return DTSExecResult.Failure;
+      }
+      if (string.IsNullOrWhiteSpace(Path))
+      {
+        return DTSExecResult.Failure;
+      }
+      if (string.IsNullOrWhiteSpace(ConnectionString))
+      {
+        return DTSExecResult.Failure;
+      }
+
+      return DTSExecResult.Success;
+    }
+
+    public override object AcquireConnection(object txn)
+    {
+      Uri connectionUri = new Uri(ConnectionString);
+      WebHDFSClient client = new WebHDFSClient(connectionUri, UserName);
+
+      return client;
+    }
+
+    public override void ReleaseConnection(object connection)
+    {
+    }
+  }
+}
---
+++ SSISHDFS.HDFSDestination/HDFSDestination.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.SqlServer.Dts.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSISHDFS.HDFSDestination
+{
+  [DtsPipelineComponent(DisplayName="HDFS Destination", ComponentType = ComponentType.DestinationAdapter,
+    Description="Destination component for HDFS")]
+  public class HDFSDestination : PipelineComponent
+  {
+  }
+}
